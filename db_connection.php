bc7a0581613e4778537a1bd84b81f5713de9bd14
Brentus <brentaldhee06@gmail.com>
2026-06-27 16:39:50 +0800

    Small Fix

--- db_connection.php
+++ db_connection.php
@@ -18,9 +18,9 @@ $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 // Connection options
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // Throw exceptions on error
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // Fetch associative array by default
-    PDO::ATTR_EMULATE_PREPARES   => false,                  // Use real prepared statements
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       
+    PDO::ATTR_EMULATE_PREPARES   => false,                  
 ];
 
 try {
